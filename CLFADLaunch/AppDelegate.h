a3a76de1590552b4eec93b1db1f5f8c6f390340a
clf <clf@wifi.local>
2018-06-21 10:20:44 +0800

    Initial Commit

---
+++ CLFADLaunch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CLFADLaunch
+//
+//  Created by clf on 2018/6/21.
+//  Copyright © 2018年 com.clf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
